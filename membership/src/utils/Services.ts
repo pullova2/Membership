02b5ef59cb6dcb4a24670105118f8a17ad5318a8
Tim <neshtimo38@gmail.com>
2024-11-22 13:51:09 +0300

    update landing page

---
+++ membership/src/utils/Services.ts
@@ -0,0 +1,8 @@
+import Settings from "../assets/images/settings.png";
+import Homebeauty from "../assets/images/homebeauty.png";
+import MarketPlace from "../assets/images/marketplace.png";
+export const services = [
+  { name: "Gadget Repair", image: Settings },
+  { name: "Home Beauty Services", image: Homebeauty },
+  { name: "MarketPlace", image: MarketPlace },
+];
