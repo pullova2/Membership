0a48fc4321df7c97e9132e2dc36133627382c46f
Tim <neshtimo38@gmail.com>
2024-11-23 11:25:06 +0300

    update styles for darkmode text

--- membership/src/defaults/styles.ts
+++ membership/src/defaults/styles.ts
@@ -2,3 +2,7 @@ const flexcontainer = "flex flex-wrap";
 const flexWrap = "flex flex-wrap";
 const flexColCenter = "flex flex-wrap items-center justify-center";
 const flexRowCenter = "flex items-center justify-center";
+const darkModeTextStyles = "text-primary-dark dark:text-primary-light";
+const darkModeBodyStyles = "";
+
+export { darkModeTextStyles };
